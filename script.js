14d9f150e8e0d4bc8931a04771f568eb803b3884
denniskabatha <162292636+denniskabatha@users.noreply.github.com>
2024-04-11 03:28:39 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+const breedForm = document.getElementById('breedForm');
+        const imageContainer = document.getElementById('image-container');
+
+        breedForm.addEventListener('submit', async (event) => {
+            event.preventDefault();
+
+            const breedInput = document.getElementById('breed').value.trim();
+            if (!breedInput) {
+                alert('Please enter a breed name.');
+                return;
+            }
+
+            const breed = breedInput.toLowerCase();
+            const response = await fetch(`https://dog.ceo/api/breed/${breed}/images/random`);
+            const data = await response.json();
+
+            if (data.status === "success") {
+                const imageUrl = data.message;
+                displayImage(imageUrl);
+            } else {
+                alert('Breed not found. Please enter a valid dog breed.');
+            }
+        });
+
+        function displayImage(url) {
+            const imageElement = document.createElement('img');
+            imageElement.src = url;
+            imageElement.alt = 'Random Dog Image';
+            imageContainer.innerHTML = '';
+            imageContainer.appendChild(imageElement);
+        }
